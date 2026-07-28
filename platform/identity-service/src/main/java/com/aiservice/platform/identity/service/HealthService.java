2fbca291b4e024a97d3e2f8d97f38a6edde1abf3
Diwakar Dhawal <108503741+Diwakar-Dhawal@users.noreply.github.com>
2026-07-29 04:28:47 +0530

    adding basic identity-service

---
+++ platform/identity-service/src/main/java/com/aiservice/platform/identity/service/HealthService.java
@@ -0,0 +1,9 @@
+package com.aiservice.platform.identity.service;
+
+import com.aiservice.platform.identity.dto.response.HealthResponse;
+
+public interface HealthService {
+
+    public HealthResponse getHealth();
+
+}
